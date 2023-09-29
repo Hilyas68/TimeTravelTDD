cc05a10b5cd06a01dc6c22594c91635ec1e4c43b
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-09-29 10:29:21 +0100

    Given that the same value as travelFrom and travelTo then return '00:00'

--- src/main/java/uk/gov/dwp/TimeTravelCalculator.java
+++ src/main/java/uk/gov/dwp/TimeTravelCalculator.java
@@ -3,6 +3,6 @@ package uk.gov.dwp;
 public class TimeTravelCalculator {
 
   public String getTravelTime(String travelFromLocation, String travelToLocation) {
-    return "";
+    return "00:00";
   }
 }
--- src/test/java/uk/gov/dwp/TimeTravelCalculatorTest.java
+++ src/test/java/uk/gov/dwp/TimeTravelCalculatorTest.java
@@ -1,12 +1,17 @@
 package uk.gov.dwp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TimeTravelCalculatorTest {
 
   @Test
-  public void createClass(){
+  @DisplayName("Given that the same value as travelFrom and travelTo then return '00:00'")
+  public void givenSameToAndFromReturnZeroHourAndMinute(){
     TimeTravelCalculator timeTravelCalculator = new TimeTravelCalculator();
     String time = timeTravelCalculator.getTravelTime("", "");
+    assertEquals("00:00",  time, "Should return zero");
   }
 }
