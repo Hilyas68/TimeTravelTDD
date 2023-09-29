24afe6d913f658805446a04d95bb6545caef103a
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-09-29 11:02:31 +0100

    Refactor

--- src/test/java/uk/gov/dwp/TimeTravelCalculatorTest.java
+++ src/test/java/uk/gov/dwp/TimeTravelCalculatorTest.java
@@ -3,10 +3,13 @@ package uk.gov.dwp;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.time.DayOfWeek;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.dwp.logic.TravelTimeDataInterface;
 
@@ -20,7 +23,7 @@ public class TimeTravelCalculatorTest {
   @DisplayName("Given that the same value as travelFrom and travelTo then return '00:00'")
   public void givenSameToAndFromReturnZeroHourAndMinute() {
 
-    when(travelTimeData.getTravelTime("Leeds", "Leeds")).thenReturn("00:00");
+    givenTravelTIme("Leeds", "Leeds", "00:00");
     TimeTravelCalculator timeTravelCalculator = new TimeTravelCalculator(travelTimeData);
     String time = timeTravelCalculator.getTravelTime("Leeds", "Leeds");
     assertEquals("00:00", time, "Should return zero");
@@ -30,9 +33,14 @@ public class TimeTravelCalculatorTest {
   @DisplayName("Given destination travel time has not been submitted then return '00:00'")
   public void givenNoTravelTimeForDestinationReturnZero() {
 
-    when(travelTimeData.getTravelTime("Leeds", "Manchester")).thenReturn("00:00");
+    givenTravelTIme("Leeds", "Manchester", "00:00");
     TimeTravelCalculator timeTravelCalculator = new TimeTravelCalculator(travelTimeData);
     String time = timeTravelCalculator.getTravelTime("Leeds", "Manchester");
     assertEquals("00:00", time, "Should return zero '00:00' ");
+  }
+
+
+  private void givenTravelTIme(String travelFromLocation, String travelToLocation, String time) {
+    when(travelTimeData.getTravelTime(travelFromLocation, travelToLocation)).thenReturn(time);
   }
 }
